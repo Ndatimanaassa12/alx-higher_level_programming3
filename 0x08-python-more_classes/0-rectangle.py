f230a58dd359b8b4a60cfa6779eba26d627578e5
ndatimana assa <arthur.ndatimana22@gmail.com>
2024-01-29 04:12:14 -0800

    new

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""0-rectangle, built for Holberton project 0x08 task 0.
-"""
+'''Module for Rectangle class.'''
 
 
 class Rectangle:
-    """Empty class per task instructions, will be built upon in later tasks.
-    """
+    '''This class defines an empty Rectangle.'''
     pass
