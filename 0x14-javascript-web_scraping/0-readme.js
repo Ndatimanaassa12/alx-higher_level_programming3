42e881a34c6549835790b5b6e5024ab04e94d616
Ndatimana Assa <arthur.ndatiman22@gmail.com>
2024-07-31 03:05:55 -0700

    nn

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-
 const fs = require('fs');
+const args = process.argv.slice(2)[0];
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
+fs.readFile(args, 'utf-8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
 });
