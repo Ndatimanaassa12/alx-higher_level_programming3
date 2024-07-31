fdc50793929823b2ce9e254c0e2e79667cb5267e
ndatimana assa <arthur.ndatimana22@gmail.com>
2024-07-31 11:35:28 -0700

    new

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const args = process.argv.slice(2)[0];
-
-fs.readFile(args, 'utf-8', (err, data) => {
-  if (err) console.log(err);
-  else console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
